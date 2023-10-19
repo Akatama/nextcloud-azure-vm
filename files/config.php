7feb6ed40fb7e935e790bc69f9523caf3e083621
Akatama <jimmyl@pacbell.net>
2023-10-18 23:09:44 -0700

    Working ont the Bicep file creation, might rename it to something else later

---
+++ files/config.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc4asc25kjhn',
+  'passwordsalt' => 'G5u0bZvRVHZjVKCgjFbFxVh3VQQz+B',
+  'secret' => 'Us8OMSPbISm2nJx5CVaNEQtKtHG3n3j6BV+wKrKbyKIJ9PEO',
+  'trusted_domains' =>
+  array (
+          0 => 'localhost',
+          1 => '*'
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '27.1.2.1',
+  'overwrite.cli.url' => 'https://40.122.118.178/nextcloud',
+  'htaccess.RewriteBase' => '/nextcloud',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'ncadmin',
+  'dbpassword' => 'slapstick-pretzel-sesame-deputize',
+  'installed' => true,
+  "maintenance_window_start" => "1",
+);
